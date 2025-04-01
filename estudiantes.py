eba9d7bb9c2845ae14045298ccb90a4decd9d2c9
sofiaycamil <estudiantessoficamil@gmail.com>
2025-04-01 13:19:52 -0300

    Add files via upload

---
+++ estudiantes.py
@@ -0,0 +1,16 @@
+estudiantes= {
+       "Juan Perez":{ "edad":17 ,"materias":["matematicas","Fisica"]},
+       "Ana gomez": {"edad": 16 ,"materias":["quimica","historia"]},
+        "Pedro López": {"edad": 18, "materias": ["Biología", "Inglés"]}
+}
+
+def nuevo_estudiante():
+  nombre=input("ingrese su nombre")
+  edad=int(input("ingrese su edad")) 
+
+
+#menu
+#imprimir la opciones
+#ingresar la opción
+#validar la entrada de opciones con el match case
+# una vez validada llamamos a cada función
