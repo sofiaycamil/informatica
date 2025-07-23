8000cf22f68c059c734538c24686134e49f78fdf
sofiaycamil <estudiantessoficamil@gmail.com>
2025-07-23 18:43:34 -0300

    Add files via upload

--- prueba.py
+++ prueba.py
@@ -4,7 +4,40 @@ ventana=Tk()
 ventana.title("ejercicio")
 ventana.geometry("400x400")
 ventana.config(bd=24)
+def sumar():
+    try:
+        resultado.set(float(numero1.get())+ float(numero2.get()))
+    except:
+        messagebox.showerror("Error","Introduce bien los datos")
+        numero1.set("")
+        numero2.set("")
 
+def restar():
+    try:
+        resultado.set(float(numero1.get()) - float(numero2.get()))
+        mostrarResultado()
+    except:
+        messagebox.showwerror("Error","Introducce bien los datos ")
+        numero1.set("")
+        numero2.set("")
+
+def multiplicar():
+    try:
+         resultado.set(float(numero1.get()) * float(numero2.get()))
+         mostrarResultado()
+    except:
+        messagebox.showwerror("Error","Introducce bien los datos ")
+        numero1.set("")
+        numero2.set("")
+
+def dividir():
+    try:
+        resultado.set(float(numero1.get())/float(numero2.get()))
+        mostrarRsulatdo()
+    except:
+        messagebox.showwerror("Error","Introducce bien los datos ")
+        numero1.set("")
+        numero2.set("")
 
 numero1=StringVar()
 numero2=StringVar()
@@ -25,4 +58,8 @@ Entry(marco,textvariable=numero1,justify="center").pack()
 Label(marco,text="segundo numero:").pack()
 Entry(marco,textvariable=numero2,justify="center").pack()
 
+Button(marco,text="sumar",command=sumar).pack(side="left",fill=X, expand=YES)
+Button(marco,text="restar",command=restar).pack(side="left",fill=X, expand=YES)
+Button(marco,text="dividir",command=dividir).pack(side="left",fill=X, expand=YES)
+Button(marco,text="multiplicar",command=multiplicar).pack(side="left",fill=X, expand=YES)
 ventana.mainloop()
