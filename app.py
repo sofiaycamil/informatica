7b8ae9f874ad6edc471786126e0eaa2ead7ba79a
sofiaycamil <estudiantessoficamil@gmail.com>
2025-10-22 14:13:21 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -6,3 +6,15 @@ def get_db_connection():
     conn=sqlite3.connect('database.db')
     conn.row_factory=sqlite3.row
     return conn
+#ruta princial de nuestra app
+@app.route('/') 
+def index():
+    conn=get_db_connection()
+    rows=conn.execute('select*from fake')
+    conn.close()
+    return render_template('index_html',rows=rows)
+if __name__=='__name__':
+ app.run(debug=True)
+
+
+  
